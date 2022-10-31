9cf686de88d497c7497b76e48b7f693707e11dd7
Mehmet Faahem <mehmetfaahem@gmail.com>
2022-10-31 22:17:53 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const app = express();
+const PORT = 4000;
+
+const fs = require('fs')
+const messageData = fs.readFileSync('messages.json')
+
+const messagesData = JSON.parse(messageData)
+
+const http = require('http').Server(app);
+const cors = require('cors');
+
+app.use(cors());
+
+const socketIO = require('socket.io')(http, {
+    cors: {
+        origin: "http://localhost:3001"
+    }
+});
+
+let users = [];
+
+socketIO.on('connection', (socket) => {
+    console.log(`⚡: ${socket.id} user just connected!`);
+
+    socket.on('friendID', (data) => {
+        socketIO.emit('clickedID', data);
+    })
+
+    // socket.on('message', (data) => {
+    //     if (data.clickedID === '') {
+    //         socketIO.emit('messageResponse', data);
+    //     } else {
+    //         socketIO.to(data.clickedID).emit('messageResponse', data)
+    //     }
+    // });
+
+    socket.on("message", data => {
+        messagesData["messages"].push(data)
+        const stringData = JSON.stringify(messagesData, null, 2)
+        fs.writeFile("messages.json", stringData, (err) => {
+            console.error(err)
+        })
+        socketIO.emit("messageResponse", data)
+    })
+
+
+
+    socket.on('joining-room', room => {
+        socket.join(room)
+    })
+
+
+    socket.on('typing', (data) => socket.broadcast.emit('typingResponse', data));
+
+    socket.on('newUser', (data) => {
+        users.push(data);
+        socketIO.emit('newUserResponse', users);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('🔥: A user disconnected');
+        users = users.filter((user) => user.socketID !== socket.id);
+        socketIO.emit('newUserResponse', users);
+        socket.disconnect();
+    });
+});
+
+app.get('/api', (req, res) => {
+    res.json(messagesData);
+});
+
+http.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+});
